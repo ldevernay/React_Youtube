b377d69d90b0dbd6dd81193da12ac451cc7d5e7c
ldevernay <boblehamster@yahoo.fr>
2017-09-15 16:39:14 +0200

    init SearchBar input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SearchBar from "./components/search_bar";
+
 const API_KEY = "AIzaSyDDxRI49Z9RoW_HLFXhVUiTk8sHByG_Ip0";
 
 const App = () => {
-  return <div>Hi!</div>;
+  return <SearchBar />;
 };
 
 ReactDOM.render(<App />, document.querySelector(".container"));
