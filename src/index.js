43792b3e9731af0128d6d78d60bccfabac391269
ldevernay <boblehamster@yahoo.fr>
2017-09-15 16:28:44 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const API_KEY = "AIzaSyDDxRI49Z9RoW_HLFXhVUiTk8sHByG_Ip0";
+
+const App = () => {
+  return <div>Hi!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector(".container"));
