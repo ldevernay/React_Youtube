856a2d7150afcc17dbc9db2e985b7c107f07fb91
ldevernay <boblehamster@yahoo.fr>
2017-11-30 10:21:00 +0100

    Fetch video list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
 const API_KEY = "AIzaSyDDxRI49Z9RoW_HLFXhVUiTk8sHByG_Ip0";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({
+      key: API_KEY,
+      term: 'little pony'
+    }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos }/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
