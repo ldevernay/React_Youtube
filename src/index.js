27c15c17eaf64173a7cd6125d318e9084de736ec
ldevernay <boblehamster@yahoo.fr>
2017-11-30 11:00:52 +0100

    Video details

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-import YTSearch from 'youtube-api-search';
+import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = "AIzaSyDDxRI49Z9RoW_HLFXhVUiTk8sHByG_Ip0";
 
@@ -28,7 +29,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={ this.state.videos }/>
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
