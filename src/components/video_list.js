12e06b72dbbebaf3a6438f87a7b958a1ce9916f5
ldevernay <boblehamster@yahoo.fr>
2017-11-30 10:34:15 +0100

    Added key

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,7 @@ import VideoListItem from './video_list_item';
 const VideoList = (props) => {
     const videoItems = props.videos.map((video) => {
         return (
-            <VideoListItem video={video} />
+            <VideoListItem key={video.etag} video={video} />
         )
     });
 
